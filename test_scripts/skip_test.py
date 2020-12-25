d55a98f644d61c3e96fe7c0ba6a2b2edb5318bb5
DarkGeekMS <mohamedshawky911@gmail.com>
2020-12-26 00:53:13 +0200

    add output and termination tests

--- test_scripts/skip_test.py
+++ test_scripts/skip_test.py
@@ -23,6 +23,7 @@ out_ports = ["tcp://127.0.0.1:5570",
             "tcp://127.0.0.1:5574",
             "tcp://127.0.0.1:5575"]
 
+# define pytest fixture to allocate in ports
 @pytest.fixture()
 def init_in_socket():
     context = zmq.Context()
@@ -30,6 +31,7 @@ def init_in_socket():
     zmq_socket.bind(in_ports[port_counter])
     return zmq_socket
 
+# define pytest fixture to allocate out ports
 @pytest.fixture()
 def init_out_socket():
     context = zmq.Context()
@@ -37,33 +39,77 @@ def init_out_socket():
     zmq_socket.bind(out_ports[port_counter])
     return zmq_socket
 
+# OTSU node skip test class
 class TestSkipOTSU:
 
     @pytest.fixture(autouse=True)
     def _init_test_sockets(self, init_in_socket, init_out_socket):
+        # pytest fixture to set in and out sockets
         self.in_socket = init_in_socket
         self.out_socket = init_out_socket
 
     def test_connection(self):
+        # test OSTU node connection
         global port_counter
+        # read RGB image as greyscale
         img = cv2.imread('back_machine/inputs/test_image.png', cv2.IMREAD_GRAYSCALE)
+        # create a thread for OTSU node
         thread = Thread(target = consumer, args = (in_ports[port_counter], out_ports[port_counter], 1, True))
+        # start OTSU node thread
         thread.start()
+        # send greyscale image through input socket
         in_message = { 'frame' : img }
         self.in_socket.send_pyobj(in_message)
+        # join OTSU node thread
         thread.join()
         port_counter += 1
+        # read return message from output socket
         ret_message = self.out_socket.recv_pyobj()
         frame = ret_message['binary']
+        # assert type of message
         assert type(frame).__module__ == np.__name__, "OTSU node return is invalid!"
 
     def test_output(self):
+        # test OSTU node output validity
         global port_counter
+        # read RGB image as greyscale
+        img = cv2.imread('back_machine/inputs/test_image.png', cv2.IMREAD_GRAYSCALE)
+        # threshold greyscale image
+        threshold, binary = cv2.threshold(img , 0 , 255 , cv2.THRESH_OTSU)
+        # create a thread for OTSU node
+        thread = Thread(target = consumer, args = (in_ports[port_counter], out_ports[port_counter], 1, True))
+        # start OTSU node thread
+        thread.start()
+        # send greyscale image through input socket
+        in_message = { 'frame' : img }
+        self.in_socket.send_pyobj(in_message)
+        # join OTSU node thread
+        thread.join()
         port_counter += 1
+        # read return message from output socket
+        ret_message = self.out_socket.recv_pyobj()
+        frame = ret_message['binary']
+        # assert value of message
+        assert (binary == frame).all(), "The output image of OTSU node is incorrect!"
 
     def test_terminate(self):
+        # test OSTU node output validity
         global port_counter
+        # create a thread for OTSU node
+        thread = Thread(target = consumer, args = (in_ports[port_counter], out_ports[port_counter], 1, True))
+        # start OTSU node thread
+        thread.start()
+        # send an empty list through input socket
+        in_message = { 'frame' : list() }
+        self.in_socket.send_pyobj(in_message)
+        # join OTSU node thread
+        thread.join()
         port_counter += 1
+        # read return message from output socket
+        ret_message = self.out_socket.recv_pyobj()
+        frame = ret_message['binary']
+        # assert value of message
+        assert len(frame) == 0, "The output is not empty upon termination!"
     
     def test_wrong_input(self):
         global port_counter
