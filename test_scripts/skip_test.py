5761dcb5cf268750866a8ea29e1901768dfa2596
DarkGeekMS <mohamedshawky911@gmail.com>
2020-12-26 02:43:36 +0200

    complete tests implementation

--- test_scripts/skip_test.py
+++ test_scripts/skip_test.py
@@ -4,6 +4,7 @@ This script is used to test the skip marker feature on OTSU node module
 import pytest
 import cv2
 import zmq
+import time
 import numpy as np
 from threading import Thread
 from back_machine.ostu_node import consumer
@@ -57,12 +58,14 @@ class TestSkipOTSU:
         thread = Thread(target = consumer, args = (in_ports[port_counter], out_ports[port_counter], 1, True))
         # start OTSU node thread
         thread.start()
+        port_counter += 1
         # send greyscale image through input socket
         in_message = { 'frame' : img }
         self.in_socket.send_pyobj(in_message)
+        # assert whether the thread is dead
+        assert thread.is_alive(), "OTSU node thread died, function failed!"
         # join OTSU node thread
         thread.join()
-        port_counter += 1
         # read return message from output socket
         ret_message = self.out_socket.recv_pyobj()
         frame = ret_message['binary']
@@ -80,12 +83,14 @@ class TestSkipOTSU:
         thread = Thread(target = consumer, args = (in_ports[port_counter], out_ports[port_counter], 1, True))
         # start OTSU node thread
         thread.start()
+        port_counter += 1
         # send greyscale image through input socket
         in_message = { 'frame' : img }
         self.in_socket.send_pyobj(in_message)
+        # assert whether the thread is dead
+        assert thread.is_alive(), "OTSU node thread died, function failed!"
         # join OTSU node thread
         thread.join()
-        port_counter += 1
         # read return message from output socket
         ret_message = self.out_socket.recv_pyobj()
         frame = ret_message['binary']
@@ -99,12 +104,14 @@ class TestSkipOTSU:
         thread = Thread(target = consumer, args = (in_ports[port_counter], out_ports[port_counter], 1, True))
         # start OTSU node thread
         thread.start()
+        port_counter += 1
         # send an empty list through input socket
         in_message = { 'frame' : list() }
         self.in_socket.send_pyobj(in_message)
+        # assert whether the thread is dead
+        assert thread.is_alive(), "OTSU node thread died, function failed!"
         # join OTSU node thread
         thread.join()
-        port_counter += 1
         # read return message from output socket
         ret_message = self.out_socket.recv_pyobj()
         frame = ret_message['binary']
@@ -112,13 +119,41 @@ class TestSkipOTSU:
         assert len(frame) == 0, "The output is not empty upon termination!"
     
     def test_wrong_input(self):
+        # test OTSU node with invalid input type
         global port_counter
+        # create a thread for OTSU node
+        thread = Thread(target = consumer, args = (in_ports[port_counter], out_ports[port_counter], 1, True))
+        # start OTSU node thread
+        thread.start()
         port_counter += 1
+        # send an integer through input socket (invalid input type)
+        in_message = { 'frame' : 1 }
+        self.in_socket.send_pyobj(in_message)
+        # assert whether the thread is dead
+        time.sleep(5)
+        assert thread.is_alive(), "OTSU node thread died, incorrect input!"
+        # join OTSU node thread
+        thread.join()
+        # read return message from output socket
+        ret_message = self.out_socket.recv_pyobj()
+        frame = ret_message['binary']
 
     def test_wrong_port(self):
+        # test OTSU node with invalid port
         global port_counter
+        # create a thread for OTSU node (don't pass output port)
+        thread = Thread(target = consumer, args = (in_ports[port_counter], None, 1, True))
+        # start OTSU node thread
+        thread.start()
         port_counter += 1
-
-    def test_occupied_port(self):
-        global port_counter
-        port_counter += 1
+        # send an empty list through input socket
+        in_message = { 'frame' : list() }
+        self.in_socket.send_pyobj(in_message)
+        # assert whether the thread is dead
+        time.sleep(5)
+        assert thread.is_alive(), "OTSU node thread died, incorrect input!"
+        # join OTSU node thread
+        thread.join()
+        # read return message from output socket
+        ret_message = self.out_socket.recv_pyobj()
+        frame = ret_message['binary']
